d72ac1c839dcfd54375398a643846fdb4f870f86
mrosano <mrosano@unlam.edu.ar>
2025-06-27 14:35:17 -0300

    Añade Enemigo y lasers

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,16 +1,21 @@
 extends Node2D
 
-@onready var player_spawn = $PlayerSpawn
-
-var player = null
+@onready var player_spawn_pos: Marker2D = $PlayerSpawnPos
+@onready var player: CharacterBody2D = $Player
+@onready var laser_container: Node2D = $LaserContainer
 
 func _ready():
-	player = get_tree().get_first_node_in_group("player")
 	assert(player!=null)
-	player.global_position = player_spawn.global_position
+	player.global_position = player_spawn_pos.global_position
+	player.laser_shot.connect(_on_player_laser_shot)
 
 func _process(_delta):
 	if Input.is_action_just_pressed("Quit"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("Reset"):
 		get_tree().reload_current_scene()
+
+func _on_player_laser_shot(lascer_scene, location):
+	var laser = lascer_scene.instantiate()
+	laser.global_position = location
+	laser_container.add_child(laser)
---
+++ Scripts/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@export var speed = 300
+@export var firing_rate = 0.25
+
+@onready var aim = $Aim
+
+signal laser_shot(lascer_scene, location)
+
+var lascer_scene = preload("res://Scenes/laser.tscn")
+var shoot_cd := false
+
+
+func _process(delta):
+	if Input.is_action_pressed("shoot"):
+		if(!shoot_cd):
+			shoot_cd = true
+			shoot()
+			await get_tree().create_timer(firing_rate).timeout
+			shoot_cd = false
+
+func _physics_process(_delta):
+	var direction = Vector2(Input.get_axis("move_left","move_right"),Input.get_axis("move_up","move_down"))
+	velocity = direction * speed
+	move_and_slide()
+
+func shoot():
+	laser_shot.emit(lascer_scene, aim.global_position)
